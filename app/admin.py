a8c0424d4b44daeeb42683744f944629b9d99eea
Yeddula Dastagri <yedduladastagiri999@gmail.com>
2024-12-25 12:14:49 +0530

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here. 
-from app.models import * 
+from app.models import *
 
 admin.site.register(Customer) 
 admin.site.register(Product)
